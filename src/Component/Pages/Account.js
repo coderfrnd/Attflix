19e761bcc1e76d7917fb486354ffd430a62426c3
attacker frnd <warriorsshadow39@gmail.com>
2023-06-06 22:53:50 +0530

    ft commit

--- src/Component/Pages/Account.js
+++ src/Component/Pages/Account.js
@@ -10,9 +10,7 @@ const Account = () => {
    <div className='absolute top-[20%] p-4 md:p-8'>
    <h1 className='text-3xl md:text-5xl
    font-bold'>My Shows</h1>
-
    </div>
-
    </div>
     <Savedshows/>
     </>
--- src/Component/Savedshows.js
+++ src/Component/Savedshows.js
@@ -1,69 +1,84 @@
 import React, { useEffect, useState } from 'react';
-import { MdChevronLeft,MdChevronRight} from 'react-icons/md';
+import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
 import { UserAuth } from '../Context/Authcontext';
 import { db } from '../Firebase';
-import { doc,updateDoc,onSnapshot } from 'firebase/firestore';
-import {AiOutlineClose} from 'react-icons/ai'
+import { doc, updateDoc, onSnapshot } from 'firebase/firestore';
+import { AiOutlineClose } from 'react-icons/ai';
 
 const Savedshows = () => {
-    const {user}=UserAuth()
-    const [movies, setmovies] = useState([])
-    const slideleft = ()=>{
-        var slider=document.getElementById('slider');
-        slider.scrollLeft=slider.scrollLeft+500;
-      }
-      const slideRight = ()=>{
-        var slider=document.getElementById('slider');
-        slider.scrollLeft=slider.scrollLeft+500;
-        // slider.scr
-      };
-      useEffect(() => {
-        const docRef = doc(db, 'users', `${user?.email}`);
-        const unsubscribe = onSnapshot(docRef, (doc) => {
-          setmovies(doc.data()?.savedshows);
-        });
-        
-        // Cleanup function to stop listening for changes to the document
-        return () => unsubscribe();
-      }, [user?.email]);
-      const movieRef = doc(db,'users' ,`${user?.email}`)
-      const deleteShow = async(passedID)=>{
-        try{
-           const result = movies.filter((it)=>it.id!==passedID)
-           await updateDoc(movieRef,{
-            savedshows:result,
-           });
-        }catch(error){
-            console.log(error)
-        }
+  const { user } = UserAuth();
+  const [movies, setMovies] = useState([]);
 
-      }
-      
+  const slideleft = () => {
+    var slider = document.getElementById('slider');
+    slider.scrollLeft = slider.scrollLeft + 500;
+  };
+
+  const slideRight = () => {
+    var slider = document.getElementById('slider');
+    slider.scrollLeft = slider.scrollLeft + 500;
+  };
+
+  useEffect(() => {
+    const docRef = doc(db, 'users', `${user?.email}`);
+    const unsubscribe = onSnapshot(docRef, (doc) => {
+      setMovies(doc.data()?.savedshows);
+    });
+
+    // Cleanup function to stop listening for changes to the document
+    return () => unsubscribe();
+  }, [user?.email]);
+
+  const movieRef = doc(db, 'users', `${user?.email}`);
+
+  const deleteShow = async (passedID) => {
+    try {
+      const result = movies.filter((it) => it.id !== passedID);
+      await updateDoc(movieRef, {
+        savedshows: result,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <>
-     <h2 className='text-white font-bold md:text-xl p-4'>
-      My Shows
-      </h2>
-      <div className='relative flex items-center group'>
-        <MdChevronLeft onClick={slideleft} size={40} className='bg-white left-0 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block' />
-        <div id={'slider'} className='w-full h-full overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide relative'>
-          {movies.map((it, id) => (
-            <div key={id} className='w-[160px] sm:w-[200px] md:w-[240px] lg:w-[280px] inline-block cursor-pointer relative p-2'>
-            <img className='w-full h-auto block' src={`https://image.tmdb.org/t/p/w500/${it?.img}`} alt={it?.title} />
-            <div className='absolute top-0 left-0 w-full h-full hover:bg-black/80 opacity-0 hover:opacity-100 text-white'>
-                <p className='white-space-normal text-xs md:text-sm font-bold flex justify-center items-center h-full text-center'>{it?.title}</p>
-                <p onClick={()=> deleteShow(it.id)} className='absolute text-gray-300 top-4 right-4' ><AiOutlineClose/></p>
-   
-            </div>
-            </div>
-          ))}
-
+      <h2 className="text-white font-bold md:text-xl p-4">My Shows</h2>
+      <div className="relative flex items-center group">
+        <MdChevronLeft
+          onClick={slideleft}
+          size={40}
+          className="bg-white left-0 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block"
+        />
+        <div
+          id={'slider'}
+          className="w-full h-full overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide relative"
+        >
+          {movies && movies.length > 0 ? (
+            movies.map((it, id) => (
+              <div key={id} className="w-[160px] sm:w-[200px] md:w-[240px] lg:w-[280px] inline-block cursor-pointer relative p-2">
+                <img className="w-full h-auto block" src={`https://image.tmdb.org/t/p/w500/${it?.img}`} alt={it?.title} />
+                <div className="absolute top-0 left-0 w-full h-full hover:bg-black/80 opacity-0 hover:opacity-100 text-white">
+                  <p className="white-space-normal text-xs md:text-sm font-bold flex justify-center items-center h-full text-center">{it?.title}</p>
+                  <p onClick={() => deleteShow(it.id)} className="absolute text-gray-300 top-4 right-4">
+                    <AiOutlineClose />
+                  </p>
+                </div>
+              </div>
+            ))
+          ) : (
+            <p>No shows available</p>
+          )}
         </div>
-        <MdChevronRight onClick={slideRight} size={40} className='bg-white right-0 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden  group-hover:block' />
+        <MdChevronRight
+          onClick={slideRight}
+          size={40}
+          className="bg-white right-0 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden  group-hover:block"
+        />
       </div>
-
     </>
-  )
-}
+  );
+};
 
-export default Savedshows
+export default Savedshows;
